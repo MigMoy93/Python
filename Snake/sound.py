c5a918456a2eb1a712e4458ed0142a2863408639
Diego Miguel <diegomiguel.1806@gmail.com>
2026-03-24 11:54:35 +0100

    Implement fallback for sound functions on import error
    Add error handling for winsound import in sound.py

--- Snake/sound.py
+++ Snake/sound.py
@@ -1,9 +1,16 @@
-import winsound
+try:
+    import winsound
 
-# Sonido al comer
-def sound_eat():
-    winsound.PlaySound("SystemAsterisk", winsound.SND_ALIAS | winsound.SND_ASYNC)
+    # Sonido al comer
+    def sound_eat():
+        winsound.PlaySound("SystemAsterisk", winsound.SND_ALIAS | winsound.SND_ASYNC)
+            
+    # Sonido al morir
+    def sound_die():
+        winsound.PlaySound("SystemHand", winsound.SND_ALIAS | winsound.SND_ASYNC)
 
-# Sonido al morir
-def sound_die():
-    winsound.PlaySound("SystemHand", winsound.SND_ALIAS | winsound.SND_ASYNC)
+except ImportError:
+    def sound_eat():  # existe pero no hace nada
+        pass
+    def sound_die():  
+        pass
